04064014b78f7f383d12999fc0ff2f3710bc0284
simon.lawrence <simon.lawrence@bjss.com>
2020-04-17 10:55:40 +0100

    6101: initial check in of LogHelper

---
+++ src/NHSD.BuyingCatalogue.Ordering.Api/Infrastructure/ObjectExtensions.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NHSD.BuyingCatalogue.Ordering.Api.Infrastructure
+{
+    public class ObjectExtensions
+    {
+    }
+}
