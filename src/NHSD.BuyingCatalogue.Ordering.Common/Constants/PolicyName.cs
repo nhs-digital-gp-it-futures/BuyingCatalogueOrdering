c065d63bbbbb91a4c69189a5c8a1d4b0b735202e
Ruth Berry <Ruth.berry@bjss.com>
2020-04-20 13:51:17 +0100

    5482: Ordering Policy

---
+++ src/NHSD.BuyingCatalogue.Ordering.Common/Constants/PolicyName.cs
@@ -0,0 +1,8 @@
+﻿namespace NHSD.BuyingCatalogue.Ordering.Common.Constants
+{
+    public static class PolicyName
+    {
+        public const string CanAccessOrders = "CanAccessOrders";
+        public const string CanManageOrders = "CanManageOrders";
+    }
+}
