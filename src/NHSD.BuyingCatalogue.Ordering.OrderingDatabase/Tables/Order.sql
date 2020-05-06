c15e80256e04daf67ddadcb52aaac9b48a3710d6
Adrian Soomro <adrian.soomro@gmail.com>
2020-05-06 15:54:26 +0100

    Create DB Tables
    AB#6734

---
+++ src/NHSD.BuyingCatalogue.Ordering.OrderingDatabase/Tables/Order.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE dbo.[Order]
+(
+	OrderId NVARCHAR(10) NOT NULL PRIMARY KEY,
+	[Description] NVARCHAR(50) NOT NULL,
+	OrganisationId UNIQUEIDENTIFIER NOT NULL,
+	StatusId INT NOT NULL,
+	Created DATETIME2 NOT NULL,
+	LastUpdated DATETIME2 NOT NULL,
+	LastUpdatedBy UNIQUEIDENTIFIER NOT NULL
+
+	CONSTRAINT FK_Order_StatusId_OrderStatus_OrderStatusId FOREIGN KEY (StatusId) REFERENCES OrderStatus (OrderStatusId)
+)
---
+++ src/NHSD.BuyingCatalogue.Ordering.OrderingDatabase/Tables/OrderStatus.sql
@@ -0,0 +1,5 @@
+﻿CREATE TABLE dbo.OrderStatus
+(
+	OrderStatusId INT NOT NULL PRIMARY KEY, 
+    [Name] NVARCHAR(30) NULL
+)
