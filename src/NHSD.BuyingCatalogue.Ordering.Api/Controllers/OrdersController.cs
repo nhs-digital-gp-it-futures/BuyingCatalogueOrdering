b68f9d36d2e21c4af9b31bff3ef9b842dbc01bb8
Ruth Berry <Ruth.berry@bjss.com>
2020-05-06 10:13:48 +0100

    4826: Canned Data - GET

---
+++ src/NHSD.BuyingCatalogue.Ordering.Api/Controllers/OrdersController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using NHSD.BuyingCatalogue.Ordering.Api.Models;
+
+namespace NHSD.BuyingCatalogue.Ordering.Api.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    [Produces("application/json")]
+    [AllowAnonymous]
+    public sealed class OrdersController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult GetOrders()
+        {
+            var orders = new OrdersModel()
+            {
+                OrderId = new Guid(),
+                OrderDescription = "Some Order",
+                LastUpdatedBy = "Bob Smith",
+                LastUpdated = DateTime.UtcNow,
+                DateCreated = DateTime.UtcNow,
+                Status = "Unsubmitted"
+            };
+
+            return Ok(orders);
+        }
+    }
+}
---
+++ src/NHSD.BuyingCatalogue.Ordering.Api/Models/OrdersModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace NHSD.BuyingCatalogue.Ordering.Api.Models
+{
+    public sealed class OrdersModel
+    {
+        public Guid OrderId { get; set; }
+
+        public string OrderDescription { get; set; }
+
+        public string LastUpdatedBy { get; set; }
+
+        public DateTime LastUpdated { get; set; }
+
+        public DateTime DateCreated { get; set; }
+
+        public string Status { get; set; }
+    }
+}
