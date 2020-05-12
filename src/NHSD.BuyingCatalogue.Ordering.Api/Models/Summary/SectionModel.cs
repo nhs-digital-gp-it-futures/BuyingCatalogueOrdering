1c4e8e069bbcf7624f26aa7a44b76c2850e4490d
Adrian Soomro <adrian.soomro@gmail.com>
2020-05-12 15:46:37 +0100

    Get order summary canned data endpoint
    AB#6737

---
+++ src/NHSD.BuyingCatalogue.Ordering.Api/Models/Summary/SectionModel.cs
@@ -0,0 +1,9 @@
+﻿namespace NHSD.BuyingCatalogue.Ordering.Api.Models.Summary
+{
+    public sealed class SectionModel
+    {
+        public string Id { get; set; }
+
+        public string Status { get; set; }
+    }
+}
