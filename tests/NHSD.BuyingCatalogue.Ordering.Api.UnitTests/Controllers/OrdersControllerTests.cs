5f2e521b6a2cf0cb34e8df344ffd1b7409d9e77c
Ruth Berry <Ruth.berry@bjss.com>
2020-05-14 10:18:29 +0100

    5321: Get order summary

--- tests/NHSD.BuyingCatalogue.Ordering.Api.UnitTests/Controllers/OrdersControllerTests.cs
+++ tests/NHSD.BuyingCatalogue.Ordering.Api.UnitTests/Controllers/OrdersControllerTests.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Mvc;
 using Moq;
 using NHSD.BuyingCatalogue.Ordering.Api.Controllers;
 using NHSD.BuyingCatalogue.Ordering.Api.Models;
+using NHSD.BuyingCatalogue.Ordering.Api.Models.Summary;
 using NHSD.BuyingCatalogue.Ordering.Api.UnitTests.Builders;
 using NHSD.BuyingCatalogue.Ordering.Application.Persistence;
 using NHSD.BuyingCatalogue.Ordering.Domain;
@@ -96,6 +97,36 @@ namespace NHSD.BuyingCatalogue.Ordering.Api.UnitTests.Controllers
             context.OrderRepositoryMock.Verify(x => x.ListOrdersByOrganisationIdAsync(Guid.Empty), Times.Once);
         }
 
+        [Test]
+        public async Task GetOrderSummaryAsync_OrderIdDoesNotExist_ReturnsNotFound()
+        {
+            var context = OrdersControllerTestContext.Setup();
+
+            using var controller = context.OrdersController;
+
+            var response = await controller.GetOrderSummaryAsync("INVALID");
+            response.Should().BeEquivalentTo(new NotFoundResult());
+        }
+
+        [TestCase("")]
+        [TestCase(" ")]
+        [TestCase("Some Description")]
+        public async Task GetOrderSummaryAsync_IsSummaryComplete_ReturnResult(string description)
+        {
+            var orderId = "C0000014-01";
+
+            var order = CreateOrderSummaryTestData(orderId, description);
+
+            var context = OrdersControllerTestContext.Setup();
+            context.Order = order.order;
+
+            using var controller = context.OrdersController;
+
+            var result = await controller.GetOrderSummaryAsync(orderId) as OkObjectResult;
+            var orderSummary = result.Value as OrderSummaryModel;
+            orderSummary.Should().BeEquivalentTo(order.expectedSummary);
+        }
+
         private static (Order order, OrderModel expectedOrder) CreateOrderTestData(string orderId, Guid organisationId, string description)
         {
             var repositoryOrder = OrderBuilder
@@ -117,6 +148,39 @@ namespace NHSD.BuyingCatalogue.Ordering.Api.UnitTests.Controllers
                 });
         }
 
+        private static (Order order, OrderSummaryModel expectedSummary) CreateOrderSummaryTestData(string orderId, string description)
+        {
+            var repositoryOrder = OrderBuilder
+                .Create()
+                .WithOrderId(orderId)
+                .WithDescription(description)
+                .Build();
+
+            return (order: repositoryOrder,
+                expectedSummary: new OrderSummaryModel
+                {
+                    OrderId = repositoryOrder.OrderId,
+                    OrganisationId = repositoryOrder.OrganisationId,
+                    Description = repositoryOrder.Description,
+                    Sections = new List<SectionModel>()
+                    {
+                        new SectionModel
+                        {
+                            Id = "ordering-description",
+                            Status = string.IsNullOrWhiteSpace(repositoryOrder.Description) ? "incomplete" : "complete"
+                        },
+                        new SectionModel {Id = "ordering-party", Status = "incomplete"},
+                        new SectionModel {Id = "supplier", Status = "incomplete"},
+                        new SectionModel {Id = "commencement-date", Status = "incomplete"},
+                        new SectionModel {Id = "associated-services", Status = "incomplete"},
+                        new SectionModel {Id = "service-recipients", Status = "incomplete"},
+                        new SectionModel {Id = "catalogue-solutions", Status = "incomplete"},
+                        new SectionModel {Id = "additional-services", Status = "incomplete"},
+                        new SectionModel {Id = "funding-source", Status = "incomplete"}
+                    }
+                });
+        }
+
         private sealed class OrdersControllerTestContext
         {
             private OrdersControllerTestContext()
@@ -127,6 +191,8 @@ namespace NHSD.BuyingCatalogue.Ordering.Api.UnitTests.Controllers
                 OrderRepositoryMock.Setup(x => x.ListOrdersByOrganisationIdAsync(It.IsAny<Guid>()))
                     .ReturnsAsync(() => Orders);
 
+                OrderRepositoryMock.Setup(x => x.GetOrderByIdAsync(It.IsAny<string>())).ReturnsAsync(() => Order);
+
                 OrdersController = new OrdersController(OrderRepositoryMock.Object);
             }
 
@@ -134,12 +200,19 @@ namespace NHSD.BuyingCatalogue.Ordering.Api.UnitTests.Controllers
 
             internal IEnumerable<Order> Orders { get; set; }
 
+            internal Order Order { get; set; }
+
             internal OrdersController OrdersController { get; }
 
             internal static OrdersControllerTestContext Setup()
             {
                 return new OrdersControllerTestContext();
             }
+        }
+
+        private static void AssertSectionComplete(string status, bool shouldBeComplete)
+        {
+            status.Should().Be(shouldBeComplete ? "complete" : "incomplete");
         }
     }
 }
