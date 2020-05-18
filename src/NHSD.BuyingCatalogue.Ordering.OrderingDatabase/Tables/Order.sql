de42c3d51b7dfdc3232367f041850f1c459026fc
Ruth Berry <Ruth.berry@bjss.com>
2020-05-18 16:14:08 +0100

    5322: Unatabify db table

--- src/NHSD.BuyingCatalogue.Ordering.OrderingDatabase/Tables/Order.sql
+++ src/NHSD.BuyingCatalogue.Ordering.OrderingDatabase/Tables/Order.sql
@@ -1,13 +1,13 @@
 ﻿CREATE TABLE dbo.[Order]
 (
-	OrderId NVARCHAR(10) NOT NULL PRIMARY KEY,
-	[Description] NVARCHAR(50) NOT NULL,
-	OrganisationId UNIQUEIDENTIFIER NOT NULL,
-	OrderStatusId INT NOT NULL,
-	Created DATETIME2 NOT NULL,
-	LastUpdated DATETIME2 NOT NULL,
-	LastUpdatedBy UNIQUEIDENTIFIER NOT NULL,
+    OrderId NVARCHAR(10) NOT NULL PRIMARY KEY,
+    [Description] NVARCHAR(50) NOT NULL,
+    OrganisationId UNIQUEIDENTIFIER NOT NULL,
+    OrderStatusId INT NOT NULL,
+    Created DATETIME2 NOT NULL,
+    LastUpdated DATETIME2 NOT NULL,
+    LastUpdatedBy UNIQUEIDENTIFIER NOT NULL,
     LastUpdatedByName NVARCHAR(256) NULL
 
-	CONSTRAINT FK_Order_OrderStatusId_OrderStatus_OrderStatusId FOREIGN KEY (OrderStatusId) REFERENCES OrderStatus (OrderStatusId)    
+    CONSTRAINT FK_Order_OrderStatusId_OrderStatus_OrderStatusId FOREIGN KEY (OrderStatusId) REFERENCES OrderStatus (OrderStatusId)    
 )
