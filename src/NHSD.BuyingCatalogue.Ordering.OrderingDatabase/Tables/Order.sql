e82a7619e617cce91f72de2685b1798f26feb614
Ruth Berry <Ruth.berry@bjss.com>
2020-05-18 16:11:30 +0100

    5322: user name db change

--- src/NHSD.BuyingCatalogue.Ordering.OrderingDatabase/Tables/Order.sql
+++ src/NHSD.BuyingCatalogue.Ordering.OrderingDatabase/Tables/Order.sql
@@ -6,8 +6,8 @@
 	OrderStatusId INT NOT NULL,
 	Created DATETIME2 NOT NULL,
 	LastUpdated DATETIME2 NOT NULL,
-	LastUpdatedBy UNIQUEIDENTIFIER NOT NULL
+	LastUpdatedBy UNIQUEIDENTIFIER NOT NULL,
+    LastUpdatedByName NVARCHAR(256) NULL
 
-	CONSTRAINT FK_Order_OrderStatusId_OrderStatus_OrderStatusId FOREIGN KEY (OrderStatusId) REFERENCES OrderStatus (OrderStatusId), 
-    [LastUpdatedName] NVARCHAR(256) NULL
+	CONSTRAINT FK_Order_OrderStatusId_OrderStatus_OrderStatusId FOREIGN KEY (OrderStatusId) REFERENCES OrderStatus (OrderStatusId)    
 )
