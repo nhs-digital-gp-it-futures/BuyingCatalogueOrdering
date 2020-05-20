9fa50d61d7af3df9592055c916fbe2a9f1eaf5af
simon.lawrence <simon.lawrence@bjss.com>
2020-05-20 15:15:54 +0100

    7205: AB#7205 added contact table to ordering database

--- src/NHSD.BuyingCatalogue.Ordering.OrderingDatabase/Deployment/CreateTables.sql
+++ src/NHSD.BuyingCatalogue.Ordering.OrderingDatabase/Deployment/CreateTables.sql
@@ -1,2 +1,3 @@
 ﻿:r ../Tables/OrderStatus.sql
 :r ../Tables/Order.sql
+:r ../Tables/Contact.sql
---
+++ src/NHSD.BuyingCatalogue.Ordering.OrderingDatabase/Tables/Contact.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Contact]
+(
+	[ContactId] INT NOT NULL PRIMARY KEY, 
+    [Name] VARCHAR(100) NULL, 
+    [Email] VARCHAR(100) NULL, 
+    [Phone] VARCHAR(35) NULL
+)
