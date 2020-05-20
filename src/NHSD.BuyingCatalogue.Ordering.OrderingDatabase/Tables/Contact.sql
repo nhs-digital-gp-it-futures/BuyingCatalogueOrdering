8b0d7d85a419bf058912bfa56561d35cceffd98f
SimonLLawrence <63393490+SimonLLawrence@users.noreply.github.com>
2020-05-20 15:19:32 +0100

    Update Contact.sql

--- src/NHSD.BuyingCatalogue.Ordering.OrderingDatabase/Tables/Contact.sql
+++ src/NHSD.BuyingCatalogue.Ordering.OrderingDatabase/Tables/Contact.sql
@@ -1,6 +1,6 @@
 ﻿CREATE TABLE [dbo].[Contact]
 (
-	[ContactId] INT NOT NULL PRIMARY KEY, 
+    [ContactId] INT NOT NULL PRIMARY KEY, 
     [Name] VARCHAR(100) NULL, 
     [Email] VARCHAR(100) NULL, 
     [Phone] VARCHAR(35) NULL
