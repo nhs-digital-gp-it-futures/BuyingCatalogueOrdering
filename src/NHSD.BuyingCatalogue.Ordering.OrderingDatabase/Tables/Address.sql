d6624c5d9302e821a3da189bac38237cfe2aa6cb
Ruth Berry <Ruth.berry@bjss.com>
2020-05-20 15:32:57 +0100

    4616: Address db formatting

--- src/NHSD.BuyingCatalogue.Ordering.OrderingDatabase/Tables/Address.sql
+++ src/NHSD.BuyingCatalogue.Ordering.OrderingDatabase/Tables/Address.sql
@@ -1,6 +1,6 @@
 ﻿CREATE TABLE dbo.[Address]
 (
-    AddressId INT IDENTITY(1,1) NOT NULL PRIMARY KEY, 
+    AddressId INT IDENTITY(1, 1) NOT NULL, 
     Line1 NVARCHAR(256) NULL, 
     Line2 NVARCHAR(256) NULL, 
     Line3 NVARCHAR(256) NULL, 
@@ -9,5 +9,6 @@
     Town NVARCHAR(256) NULL, 
     County NVARCHAR(256) NULL, 
     Postcode NVARCHAR(10) NULL, 
-    Country NVARCHAR(256) NULL
-)
+    Country NVARCHAR(256) NULL,
+    CONSTRAINT PK_Address PRIMARY KEY (AddressId)
+);
