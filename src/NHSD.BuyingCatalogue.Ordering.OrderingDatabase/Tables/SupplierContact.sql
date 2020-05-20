a97601693b7feb5922dbb8cf1e7a00207e8c330a
simon.lawrence <simon.lawrence@bjss.com>
2020-05-20 16:26:10 +0100

    7205: review comments  removed square brackets

--- src/NHSD.BuyingCatalogue.Ordering.OrderingDatabase/Tables/SupplierContact.sql
+++ src/NHSD.BuyingCatalogue.Ordering.OrderingDatabase/Tables/SupplierContact.sql
@@ -1,8 +1,8 @@
 ﻿CREATE TABLE dbo.SupplierContact
 (
-    [SupplierContactId] INT IDENTITY(1, 1) NOT NULL,
-    [Name] VARCHAR(100) NULL, 
-    [Email] VARCHAR(100) NULL, 
-    [Phone] VARCHAR(35) NULL,
+    SupplierContactId INT IDENTITY(1, 1) NOT NULL,
+    Name VARCHAR(100) NULL, 
+    Email VARCHAR(100) NULL, 
+    Phone VARCHAR(35) NULL,
     CONSTRAINT PK_SupplierContact PRIMARY KEY (SupplierContactId)
 );
