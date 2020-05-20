6a8fb1d949bf54057f2aff07a987e26b30b195c8
Ruth Berry <Ruth.berry@bjss.com>
2020-05-20 14:50:45 +0100

    4616: Address db add

---
+++ src/NHSD.BuyingCatalogue.Ordering.OrderingDatabase/Tables/Address.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [dbo].[Address]
+(
+	AddressId INT IDENTITY(1,1) NOT NULL PRIMARY KEY, 
+    Line1 NVARCHAR(256) NULL, 
+    Line2 NVARCHAR(256) NULL, 
+    Line3 NVARCHAR(256) NULL, 
+    Line4 NVARCHAR(256) NULL, 
+    Line5 NVARCHAR(256) NULL, 
+    Town NVARCHAR(256) NULL, 
+    County NVARCHAR(256) NULL, 
+    Postcode NVARCHAR(10) NULL, 
+    Country NVARCHAR(256) NULL
+)
