cdef2021d65020b08658f9116d1e949157542f5c
Ruth Berry <Ruth.berry@bjss.com>
2020-05-20 10:51:37 +0100

    4621: Get canned data

---
+++ src/NHSD.BuyingCatalogue.Ordering.Api/Models/SupplierModel.cs
@@ -0,0 +1,10 @@
+﻿namespace NHSD.BuyingCatalogue.Ordering.Api.Models
+{
+    public sealed class SupplierModel
+    {
+        public string SupplierId { get; set; }
+        public string Name { get; set; }
+        public AddressModel Address { get; set; }
+        public PrimaryContactModel PrimaryContact { get; set; }
+    }
+}
