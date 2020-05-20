4256622b126a738bbb68a90649545144e10c1319
Ruth Berry <Ruth.berry@bjss.com>
2020-05-20 11:28:57 +0100

    4619: Get & Put canned data

---
+++ src/NHSD.BuyingCatalogue.Ordering.Api/Controllers/CommencementDateController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc;
+using NHSD.BuyingCatalogue.Ordering.Api.Models;
+
+namespace NHSD.BuyingCatalogue.Ordering.Api.Controllers
+{
+    [Route("api/v1/orders/{orderId}/commencement-date")]
+    [ApiController]
+    [Produces("application/json")]
+    public sealed class CommencementDateController : Controller
+    {
+        [HttpGet]
+        public ActionResult Get()
+        {
+            var result = new CommencementDateModel { CommencementDate = DateTime.UtcNow };
+
+            return Ok(result);
+        }
+
+        [HttpPut]
+        public ActionResult Update([FromRoute] [Required] string orderId,
+            [FromBody] [Required] CommencementDateModel model)
+        {
+            if (orderId is null)
+            {
+                return NotFound();
+            }
+
+            if (model is null)
+            {
+                throw new ArgumentNullException(nameof(model));
+            }
+
+            return NoContent();
+        }
+    }
+}
