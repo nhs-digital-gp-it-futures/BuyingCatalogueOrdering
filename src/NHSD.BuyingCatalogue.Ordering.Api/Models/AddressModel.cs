74e06112297e4af58a498a77823f8ebcedc1eb0e
Ruth Berry <Ruth.berry@bjss.com>
2020-05-20 11:57:09 +0100

    4616: Internal classes

--- src/NHSD.BuyingCatalogue.Ordering.Api/Models/AddressModel.cs
+++ src/NHSD.BuyingCatalogue.Ordering.Api/Models/AddressModel.cs
@@ -1,6 +1,6 @@
 ﻿namespace NHSD.BuyingCatalogue.Ordering.Api.Models
 {
-    public sealed class AddressModel
+    internal sealed class AddressModel
     {
         public string Line1 { get; set; }
         public string Line2 { get; set; }
