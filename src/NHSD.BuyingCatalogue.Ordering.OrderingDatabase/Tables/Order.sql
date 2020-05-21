283fe8eee686119317905efc1443c986638bfaf9
Ruth Berry <Ruth.berry@bjss.com>
2020-05-21 11:56:19 +0100

    4616: Code review comments foreign key

--- src/NHSD.BuyingCatalogue.Ordering.OrderingDatabase/Tables/Order.sql
+++ src/NHSD.BuyingCatalogue.Ordering.OrderingDatabase/Tables/Order.sql
@@ -9,13 +9,19 @@
     OrganisationBillingAddressId INT NULL,    
     OrganisationContactId INT NULL,
     OrderStatusId INT NOT NULL,    
+    SupplierId INT NULL, 
+    SupplierName NVARCHAR(255) NULL, 
+    SupplierAddressId INT NULL, 
+    SupplierContactId INT NULL,
     Created DATETIME2 NOT NULL,
     LastUpdated DATETIME2 NOT NULL,
     LastUpdatedBy UNIQUEIDENTIFIER NOT NULL,
-    LastUpdatedByName NVARCHAR(256) NULL,     
-
-    CONSTRAINT FK_Order_OrderStatusId_OrderStatus_OrderStatusId FOREIGN KEY (OrderStatusId) REFERENCES OrderStatus (OrderStatusId), 
-    CONSTRAINT FK_Order_OrganisationAddressId_Address_AddressId FOREIGN KEY (OrganisationAddressId) REFERENCES Address (AddressId),
-    CONSTRAINT FK_Order_OrganisationBillingAddressId_Address_AddressId FOREIGN KEY (OrganisationBillingAddressId) REFERENCES Address (AddressId),
-    CONSTRAINT FK_Order_OrganisationContactId_Contact_ContactId FOREIGN KEY (OrganisationContactId) REFERENCES Contact (ContactId)
+    LastUpdatedByName NVARCHAR(256) NULL,   
+    
+    CONSTRAINT FK_Order_OrderStatus FOREIGN KEY (OrderStatusId) REFERENCES OrderStatus (OrderStatusId),    
+    CONSTRAINT FK_Order_OrganisationAddress FOREIGN KEY (OrganisationAddressId) REFERENCES [Address] (AddressId),
+    CONSTRAINT FK_Order_OrganisationBillingAddress FOREIGN KEY (OrganisationBillingAddressId) REFERENCES [Address] (AddressId),     
+    CONSTRAINT FK_Order_OrganisationContact FOREIGN KEY (OrganisationContactId) REFERENCES Contact (ContactId), 
+    CONSTRAINT FK_Order_SupplierAddress FOREIGN KEY (SupplierAddressId) REFERENCES [Address] (AddressId),     
+    CONSTRAINT FK_Order_SupplierContact FOREIGN KEY (SupplierContactId) REFERENCES Contact (ContactId)
 );
