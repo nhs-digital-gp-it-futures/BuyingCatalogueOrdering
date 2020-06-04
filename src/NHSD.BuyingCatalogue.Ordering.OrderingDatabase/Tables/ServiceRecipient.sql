14e1dc9e02b91f3a104ef8f9cc13d4538f36efaa
Ruth Berry <Ruth.berry@bjss.com>
2020-06-04 15:42:27 +0100

    7412: Service recipients spacing

--- src/NHSD.BuyingCatalogue.Ordering.OrderingDatabase/Tables/ServiceRecipient.sql
+++ src/NHSD.BuyingCatalogue.Ordering.OrderingDatabase/Tables/ServiceRecipient.sql
@@ -1,8 +1,8 @@
 ﻿CREATE TABLE dbo.ServiceRecipient
 (
-    OdsCode NVARCHAR(8) NOT NULL, 
+    OdsCode NVARCHAR(8) NOT NULL,
     OrderId NVARCHAR(10) NOT NULL,
-    [Name] NVARCHAR(256) NULL, 
+    [Name] NVARCHAR(256) NULL,
     CONSTRAINT PK_ServiceRecipient PRIMARY KEY (OdsCode),
     CONSTRAINT FK_ServiceRecipient_Order FOREIGN KEY (OrderId) REFERENCES [Order] (OrderId)
 );
