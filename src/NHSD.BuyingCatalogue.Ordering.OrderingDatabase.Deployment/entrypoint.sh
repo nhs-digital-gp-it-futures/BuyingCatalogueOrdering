d445fcc80b72aa4253b0349903042b44b2b4bf43
Adrian Soomro <adrian.soomro@gmail.com>
2020-06-05 12:19:57 +0100

    build & apply dacpac to deploy db

---
+++ src/NHSD.BuyingCatalogue.Ordering.OrderingDatabase.Deployment/entrypoint.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# database port, defaults to mssqls default port.
+PORT=${PORT:-1433}
+
+# wait for MSSQL server to start
+export STATUS=1
+i=0
+
+while [[ $STATUS -ne 0 ]] && [[ $i -lt 30 ]]; do
+    i=$i+1
+    sleep 1
+    /opt/mssql-tools/bin/sqlcmd -S $DB_SERVER,$PORT -t 1 -U sa -P $SA_PASSWORD -Q "SELECT 1;" &>/dev/null
+    STATUS=$?
+done
+
+if [ $STATUS -ne 0 ]; then
+    echo "Error: MSSQL SERVER took more than thirty seconds to start up."
+    exit 1
+fi
+
+/sqlpackage/sqlpackage /Action:publish /SourceFile:NHSD.BuyingCatalogue.Ordering.OrderingDatabase.Deployment.dacpac /TargetServerName:$DB_SERVER,$PORT /TargetDatabaseName:$DB_NAME /TargetUser:sa /TargetPassword:$SA_PASSWORD
+/opt/mssql-tools/bin/sqlcmd -S $DB_SERVER,$PORT -U sa -P $SA_PASSWORD -d $DB_NAME -I -i "PostDeployment.sql"
+
+printf "\nDatabase setup complete"
