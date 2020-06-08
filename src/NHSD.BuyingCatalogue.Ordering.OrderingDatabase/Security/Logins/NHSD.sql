b40865650494040205feb904d6ebd72f598c75d9
Adrian Soomro <adrian.soomro@gmail.com>
2020-06-08 10:36:07 +0100

    Only create login for user NHSD if it doesnt exist

--- src/NHSD.BuyingCatalogue.Ordering.OrderingDatabase/Security/Logins/NHSD.sql
+++ src/NHSD.BuyingCatalogue.Ordering.OrderingDatabase/Security/Logins/NHSD.sql
@@ -1,2 +1,7 @@
-﻿CREATE LOGIN NHSD
-WITH PASSWORD = '$(NHSD_PASSWORD)';
+﻿IF NOT EXISTS 
+    (SELECT name  
+     FROM master.sys.server_principals
+     WHERE name = 'NHSD')
+BEGIN
+    CREATE LOGIN [LoginName] WITH PASSWORD = '$(NHSD_PASSWORD)'
+END
