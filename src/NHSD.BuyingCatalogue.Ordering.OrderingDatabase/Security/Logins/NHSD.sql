af3641d844098f27cb3c3cb6098d1c1fcca59b8b
Adrian Soomro <adrian.soomro@gmail.com>
2020-06-08 10:56:40 +0100

    Correct login name

--- src/NHSD.BuyingCatalogue.Ordering.OrderingDatabase/Security/Logins/NHSD.sql
+++ src/NHSD.BuyingCatalogue.Ordering.OrderingDatabase/Security/Logins/NHSD.sql
@@ -3,5 +3,5 @@
      FROM master.sys.server_principals
      WHERE name = 'NHSD')
 BEGIN
-    CREATE LOGIN [LoginName] WITH PASSWORD = '$(NHSD_PASSWORD)'
+    CREATE LOGIN NHSD WITH PASSWORD = '$(NHSD_PASSWORD)'
 END
