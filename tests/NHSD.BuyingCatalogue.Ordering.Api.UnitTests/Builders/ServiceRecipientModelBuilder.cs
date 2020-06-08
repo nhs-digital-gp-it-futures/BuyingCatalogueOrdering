e04dd6c5299a3d31b2fbd0a97cb4f9a866127606
simon.lawrence <simon.lawrence@bjss.com>
2020-06-08 08:30:18 +0100

    7471: added missing files

---
+++ tests/NHSD.BuyingCatalogue.Ordering.Api.UnitTests/Builders/ServiceRecipientModelBuilder.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NHSD.BuyingCatalogue.Ordering.Api.Models;
+using NHSD.BuyingCatalogue.Ordering.Api.UnitTests.Builders;
+using NHSD.BuyingCatalogue.Ordering.Domain;
+
+namespace NHSD.BuyingCatalogue.Ordering.Api.UnitTests.Controllers
+{
+    internal sealed class ServiceRecipientModelBuilder
+    {
+        private readonly ServiceRecipientModel _serviceRecipientModel;
+
+        private ServiceRecipientModelBuilder()
+        {
+            _serviceRecipientModel = new ServiceRecipientModel
+            {
+                Name = "Some name",
+                OdsCode =  "Ods1"
+            };
+        }
+
+        internal static ServiceRecipientModelBuilder Create() => new ServiceRecipientModelBuilder();
+
+        internal ServiceRecipientModelBuilder WithOdsCode(string odsCode)
+        {
+            _serviceRecipientModel.OdsCode = odsCode;
+            return this;
+        }
+
+        internal ServiceRecipientModelBuilder WithName(string name)
+        {
+            _serviceRecipientModel.Name = name;
+            return this;
+        }
+
+        internal ServiceRecipientModel Build() => _serviceRecipientModel;
+    }
+}
---
+++ tests/NHSD.BuyingCatalogue.Ordering.Api.UnitTests/Builders/ServiceRecipientsModelBuilder.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NHSD.BuyingCatalogue.Ordering.Api.Models;
+using NUnit.Framework.Internal;
+
+namespace NHSD.BuyingCatalogue.Ordering.Api.UnitTests.Controllers
+{
+    internal sealed class ServiceRecipientsModelBuilder
+    {
+        private readonly ServiceRecipientsModel _serviceRecipientsModel;
+        private readonly List<ServiceRecipientModel> ServiceRecipientModelList = new List<ServiceRecipientModel>();
+        private ServiceRecipientsModelBuilder()
+        {
+            _serviceRecipientsModel = new ServiceRecipientsModel()
+            {
+                ServiceRecipients = new List<ServiceRecipientModel>()
+            };
+        }
+
+        internal static ServiceRecipientsModelBuilder Create() => new ServiceRecipientsModelBuilder();
+
+        internal ServiceRecipientsModelBuilder WithServiceRecipientModel(ServiceRecipientModel serviceRecipientModel)
+        {
+            ServiceRecipientModelList.Add(serviceRecipientModel);
+            return this;
+        }
+
+        internal ServiceRecipientsModel Build()
+        {
+            _serviceRecipientsModel.ServiceRecipients = ServiceRecipientModelList;
+            return _serviceRecipientsModel;
+        }
+
+    }
+}
