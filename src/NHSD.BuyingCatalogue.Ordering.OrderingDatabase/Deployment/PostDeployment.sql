5a0bba92e612442f17eec431fc292fed3b9b6895
Sam Roberts <53512773+sam-roberts-bjss@users.noreply.github.com>
2020-06-22 10:48:10 +0100

    Fix build issue
    AB#7826
    AB#7521

--- src/NHSD.BuyingCatalogue.Ordering.OrderingDatabase/Deployment/PostDeployment.sql
+++ src/NHSD.BuyingCatalogue.Ordering.OrderingDatabase/Deployment/PostDeployment.sql
@@ -1,6 +1,5 @@
 ﻿:r ./CreateOrderStatuses.sql
 :r ./InsertCatalogueItemTypes.sql
 :r ./InsertCataloguePriceTypes.sql
-:r ./InsertPricingUnits.sql
 :r ./InsertProvisioningTypes.sql
 :r ./InsertTimeUnits.sql
