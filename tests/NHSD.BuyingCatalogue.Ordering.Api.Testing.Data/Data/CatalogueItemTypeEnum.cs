849e6d0ce5707ba4e913a6a8cc4658cd03594e8a
RuthBerryBJSS <ruth.berry@bjss.com>
2020-09-04 11:40:29 +0100

    9283: Product type value bug
    Story: AB#9283
    Task: AB#9848

--- tests/NHSD.BuyingCatalogue.Ordering.Api.Testing.Data/Data/CatalogueItemTypeEnum.cs
+++ tests/NHSD.BuyingCatalogue.Ordering.Api.Testing.Data/Data/CatalogueItemTypeEnum.cs
@@ -7,4 +7,23 @@
         AssociatedService = 3,
         Invalid = 4
     }
+
+    public static class CatalogueItemTypeExtensions
+    {
+        public static string ToDisplayName(this CatalogueItemType timeUnit)
+        {
+            switch (timeUnit)
+            {
+                case CatalogueItemType.Solution:
+                    return "Catalogue Solution";
+                case CatalogueItemType.AdditionalService:
+                    return "Additional Service";
+                case CatalogueItemType.AssociatedService:
+                    return "Associated Service";
+                default:
+                    return null;
+
+            }
+        }
+    }
 }
