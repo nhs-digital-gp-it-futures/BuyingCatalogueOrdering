f69678292843ec06f63ce7c07230dcec0f3e11cc
bjss-chris <christopher.martin@bjss.com>
2021-03-05 09:50:22 +0000

    Disable integration tests in pipeline
    AB#11053

--- src/NHSD.BuyingCatalogue.Ordering.OrderingDatabase/Indexes/IX_ServiceInstance.sql
+++
@@ -1,9 +0,0 @@
-﻿CREATE NONCLUSTERED INDEX IX_ServiceInstance ON dbo.OrderItem
-(
-    OrderId,
-    OdsCode,
-    CatalogueItemId,
-    ParentCatalogueItemId
-)
-INCLUDE (CatalogueItemTypeId)
-WHERE CatalogueItemTypeId IN (1, 2);
