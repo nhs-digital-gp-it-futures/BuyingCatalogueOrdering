bdd27575ffb7990e28f17e1e59c2fbb2a718ff9c
bjss-chris <christopher.martin@bjss.com>
2021-03-21 11:18:04 +0000

    Fix save additional service
    AB#13274

--- src/NHSD.BuyingCatalogue.Ordering.OrderingDatabase/Tables/OrderItemRecipients.sql
+++ src/NHSD.BuyingCatalogue.Ordering.OrderingDatabase/Tables/OrderItemRecipients.sql
@@ -4,7 +4,7 @@
     CatalogueItemId nvarchar(14) NOT NULL,
     OdsCode nvarchar(8) NOT NULL,
     Quantity int NOT NULL CONSTRAINT ServiceRecipient_PositiveQuantity CHECK (Quantity > 0),
-    DeliveryDate date NOT NULL,
+    DeliveryDate date NULL,
     CONSTRAINT PK_OrderItemRecipients PRIMARY KEY (OrderId, CatalogueItemId, OdsCode),
     CONSTRAINT FK_OrderItemRecipients_OrderItem FOREIGN KEY (OrderId, CatalogueItemId) REFERENCES dbo.OrderItem (OrderId, CatalogueItemId) ON DELETE CASCADE,
     CONSTRAINT FK_OrderItemRecipients_OdsCode FOREIGN KEY (OdsCode) REFERENCES dbo.ServiceRecipient (OdsCode),
