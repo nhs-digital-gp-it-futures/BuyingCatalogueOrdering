0c8a07fcf4bb041acbfcd879c4d6adf693052dc9
bjss-chris <59572143+bjss-chris@users.noreply.github.com>
2021-04-21 12:28:15 +0100

    Fix service instance ID (#398)
    AB#13910

--- src/NHSD.BuyingCatalogue.Ordering.OrderingDatabase/Views/ServiceInstanceItem.sql
+++ src/NHSD.BuyingCatalogue.Ordering.OrderingDatabase/Views/ServiceInstanceItem.sql
@@ -1,10 +1,24 @@
 ﻿CREATE VIEW dbo.ServiceInstanceItem AS
-SELECT DISTINCT r.OrderId, r.CatalogueItemId, r.OdsCode,
-       CASE WHEN (c.CatalogueItemTypeId = 2 AND h.HasParentSolution = 0) OR s.ServiceInstanceIncrement IS NULL
-         THEN NULL
-         ELSE CONCAT('SI', s.ServiceInstanceIncrement, '-', r.OdsCode)
-       END AS ServiceInstanceId
+WITH ServiceInstanceIncrement AS
+(
+    SELECT r.OrderId, r.CatalogueItemId, r.OdsCode,
+           DENSE_RANK() OVER (
+               PARTITION BY r.OrderId, r.OdsCode
+                   ORDER BY CASE WHEN c.CatalogueItemTypeId = 1 THEN r.CatalogueItemId ELSE c.ParentCatalogueItemId END) AS ServiceInstanceIncrement
+      FROM dbo.OrderItemRecipients AS r
+           INNER JOIN dbo.CatalogueItem AS c ON c.Id = r.CatalogueItemId
+                   AND c.CatalogueItemTypeId IN (1, 2)
+       WHERE (c.ParentCatalogueItemId IS NULL OR EXISTS (
+            SELECT *
+              FROM dbo.OrderItemRecipients AS r2
+             WHERE r2.OrderId = r.OrderId
+               AND r2.OdsCode = r.OdsCode
+               AND r2.CatalogueItemId = c.ParentCatalogueItemId))
+)
+SELECT r.OrderId, r.CatalogueItemId, r.OdsCode,
+       'SI' + CAST(s.ServiceInstanceIncrement AS nvarchar(3)) + '-' + r.OdsCode AS ServiceInstanceId
   FROM dbo.OrderItemRecipients AS r
-  INNER JOIN dbo.CatalogueItem AS c ON c.Id = r.CatalogueItemId
-  CROSS APPLY dbo.GetServiceInstanceIncrement(r.OrderId, r.CatalogueItemId) AS s
-  CROSS APPLY dbo.HasParentSolution(r.OrderId, r.CatalogueItemId) AS h;
+       LEFT OUTER JOIN ServiceInstanceIncrement AS s
+               ON s.OrderId = r.OrderId
+              AND s.CatalogueItemId = r.CatalogueItemId
+              AND s.OdsCode = r.OdsCode;
