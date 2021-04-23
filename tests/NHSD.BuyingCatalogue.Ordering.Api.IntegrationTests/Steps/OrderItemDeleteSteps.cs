dae97202ac9c0b4d72ea1ed7f475b2ac51ad4943
Neha Jain <neha.jain@mastek.com>
2021-04-23 09:59:37 +0100

    funding source status bug fix

--- tests/NHSD.BuyingCatalogue.Ordering.Api.IntegrationTests/Steps/OrderItemDeleteSteps.cs
+++ tests/NHSD.BuyingCatalogue.Ordering.Api.IntegrationTests/Steps/OrderItemDeleteSteps.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Threading.Tasks;
+using FluentAssertions;
+using NHSD.BuyingCatalogue.Ordering.Api.IntegrationTests.Support;
 using NHSD.BuyingCatalogue.Ordering.Api.IntegrationTests.Utils;
+using NHSD.BuyingCatalogue.Ordering.Api.Testing.Data.Entities;
 using TechTalk.SpecFlow;
 
 namespace NHSD.BuyingCatalogue.Ordering.Api.IntegrationTests.Steps
@@ -10,14 +13,17 @@ namespace NHSD.BuyingCatalogue.Ordering.Api.IntegrationTests.Steps
     {
         private readonly Request request;
         private readonly Settings settings;
+        private readonly OrderContext orderContext;
         private DeleteOrderItemRequest deleteOrderItemRequest;
 
         public OrderItemDeleteSteps(
             Request request,
-            Settings settings)
+            Settings settings,
+            OrderContext orderContext)
         {
             this.request = request;
             this.settings = settings ?? throw new ArgumentNullException(nameof(settings));
+            this.orderContext = orderContext ?? throw new ArgumentNullException(nameof(orderContext));
         }
 
         [Given(@"the user creates a request to delete order item with catalogue item ID (.*) for the order with ID (\d{1,6})")]
@@ -36,6 +42,13 @@ namespace NHSD.BuyingCatalogue.Ordering.Api.IntegrationTests.Steps
             await deleteOrderItemRequest.ExecuteAsync();
         }
 
+        [Then(@"the fundingSourceOnlyGms value is null")]
+        public async Task ThenTheFundingSourceOnlyGmsIs()
+        {
+            var order = await OrderEntity.FetchOrderByOrderId(settings.ConnectionString, deleteOrderItemRequest.OrderId);
+            order.FundingSourceOnlyGms.Should().BeNull();
+        }
+
         private sealed class DeleteOrderItemRequest
         {
             private readonly Request request;
@@ -48,9 +61,11 @@ namespace NHSD.BuyingCatalogue.Ordering.Api.IntegrationTests.Steps
                 int orderId)
             {
                 this.request = request ?? throw new ArgumentNullException(nameof(request));
-
+                OrderId = orderId;
                 deleteOrderUrl = $"{orderingApiBaseAddress}/api/v1/orders/C0{orderId}-01/order-items/{catalogueItemId}";
             }
+
+            internal int OrderId { get; }
 
             public async Task ExecuteAsync() => await request.DeleteAsync(deleteOrderUrl);
         }
