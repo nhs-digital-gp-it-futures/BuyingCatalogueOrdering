e7b0612e54bfafddba7ae374546a5a857895c13b
RahulJha-mastek <Rahul.Jha@mastek.com>
2021-04-23 10:49:59 +0100

    Move DB functionality to service layer – SupplierSectionController

---
+++ src/NHSD.BuyingCatalogue.Ordering.Contracts/ISupplierSectionService.cs
@@ -0,0 +1,12 @@
+﻿using System.Threading.Tasks;
+using NHSD.BuyingCatalogue.Ordering.Domain;
+
+namespace NHSD.BuyingCatalogue.Ordering.Contracts
+{
+    public interface ISupplierSectionService
+    {
+        Task<Order> GetOrder(CallOffId callOffId);
+
+        Task SetSupplierSection(Order order, Supplier supplier, Contact contact);
+    }
+}
---
+++ src/NHSD.BuyingCatalogue.Ordering.Services/SupplierSectionService.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using NHSD.BuyingCatalogue.Ordering.Contracts;
+using NHSD.BuyingCatalogue.Ordering.Domain;
+using NHSD.BuyingCatalogue.Ordering.Persistence.Data;
+
+namespace NHSD.BuyingCatalogue.Ordering.Services
+{
+    public sealed class SupplierSectionService : ISupplierSectionService
+    {
+        private readonly ApplicationDbContext context;
+
+        public SupplierSectionService(ApplicationDbContext context)
+        {
+            this.context = context ?? throw new ArgumentNullException(nameof(context));
+        }
+
+        public async Task<Order> GetOrder(CallOffId callOffId)
+        {
+            return await context.Order
+                .Where(o => o.Id == callOffId.Id)
+                .Include(o => o.Supplier).ThenInclude(s => s.Address)
+                .Include(o => o.SupplierContact)
+                .SingleOrDefaultAsync();
+        }
+
+        public async Task SetSupplierSection(Order order, Supplier supplier, Contact contact)
+        {
+            if (order is null)
+                throw new ArgumentNullException(nameof(order));
+
+            if (supplier is null)
+                throw new ArgumentNullException(nameof(supplier));
+
+            if (contact is null)
+                throw new ArgumentNullException(nameof(contact));
+
+            order.Supplier ??= await context.Supplier.FindAsync(supplier.Id) ?? new Supplier
+            {
+                Id = supplier.Id,
+            };
+
+            order.Supplier.Name = supplier.Name;
+            order.Supplier.Address = supplier.Address;
+            order.SupplierContact = contact;
+
+            await context.SaveChangesAsync();
+        }
+    }
+}
