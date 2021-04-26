c1e115b7a674755adea7fa3e5115be49cbb70488
RahulJha-mastek <78419456+RahulJha-mastek@users.noreply.github.com>
2021-04-26 09:50:52 +0100

    Move DB functionality to service layer – SupplierSectionController (#404)
    * Move DB functionality to service layer – SupplierSectionController

--- tests/NHSD.BuyingCatalogue.Ordering.Api.UnitTests/Controllers/SupplierSectionControllerTests.cs
+++ tests/NHSD.BuyingCatalogue.Ordering.Api.UnitTests/Controllers/SupplierSectionControllerTests.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Diagnostics.CodeAnalysis;
-using System.Linq;
 using System.Linq.Expressions;
 using System.Threading.Tasks;
 using AutoFixture;
@@ -14,8 +13,8 @@ using NHSD.BuyingCatalogue.Ordering.Api.Controllers;
 using NHSD.BuyingCatalogue.Ordering.Api.Models;
 using NHSD.BuyingCatalogue.Ordering.Api.Services;
 using NHSD.BuyingCatalogue.Ordering.Api.UnitTests.AutoFixture;
+using NHSD.BuyingCatalogue.Ordering.Contracts;
 using NHSD.BuyingCatalogue.Ordering.Domain;
-using NHSD.BuyingCatalogue.Ordering.Persistence.Data;
 using NUnit.Framework;
 
 namespace NHSD.BuyingCatalogue.Ordering.Api.UnitTests.Controllers
@@ -38,9 +37,11 @@ namespace NHSD.BuyingCatalogue.Ordering.Api.UnitTests.Controllers
         [Test]
         [InMemoryDbAutoData]
         public static async Task GetAsync_OrderDoesNotExist_ReturnsNotFound(
+            [Frozen] Mock<ISupplierSectionService> service,
             CallOffId callOffId,
             SupplierSectionController controller)
         {
+            service.Setup(o => o.GetOrder(callOffId)).ReturnsAsync((Order)null);
             var response = await controller.GetAsync(callOffId);
 
             response.Should().NotBeNull();
@@ -50,22 +51,36 @@ namespace NHSD.BuyingCatalogue.Ordering.Api.UnitTests.Controllers
         [Test]
         [InMemoryDbAutoData]
         public static async Task GetAsync_OrderIdExists_SupplierSectionDetailsReturned(
-            [Frozen] ApplicationDbContext context,
+            [Frozen] Mock<ISupplierSectionService> service,
             [Frozen] CallOffId callOffId,
             Order order,
             SupplierSectionController controller)
         {
             order.Supplier.Should().NotBeNull();
             order.SupplierContact.Should().NotBeNull();
-
-            context.Add(order);
-            await context.SaveChangesAsync();
+            service.Setup(o => o.GetOrder(callOffId)).ReturnsAsync(order);
 
             var expectedValue = new SupplierModel(order.Supplier, order.SupplierContact);
 
             var response = await controller.GetAsync(callOffId);
 
             response.Value.Should().BeEquivalentTo(expectedValue);
+        }
+
+        [Test]
+        [InMemoryDbAutoData]
+        public static async Task GetAsync_InvokesGetOrder(
+            [Frozen] Mock<ISupplierSectionService> service,
+            [Frozen] CallOffId callOffId,
+            Order order,
+            SupplierSectionController controller)
+        {
+            order.Supplier.Should().NotBeNull();
+            service.Setup(o => o.GetOrder(callOffId)).ReturnsAsync(order);
+
+            await controller.GetAsync(callOffId);
+
+            service.Verify(o => o.GetOrder(callOffId), () => Times.AtMost(1));
         }
 
         [Test]
@@ -80,10 +95,12 @@ namespace NHSD.BuyingCatalogue.Ordering.Api.UnitTests.Controllers
         [Test]
         [InMemoryDbAutoData]
         public static async Task UpdateAsync_OrderDoesNotExist_ReturnsNotFound(
+            [Frozen] Mock<ISupplierSectionService> service,
             CallOffId callOffId,
             SupplierModel model,
             SupplierSectionController controller)
         {
+            service.Setup(o => o.GetOrder(callOffId)).ReturnsAsync((Order)null);
             var response = await controller.UpdateAsync(callOffId, model);
 
             response.Should().BeOfType<NotFoundResult>();
@@ -91,18 +108,20 @@ namespace NHSD.BuyingCatalogue.Ordering.Api.UnitTests.Controllers
 
         [Test]
         [InMemoryDbAutoData]
-        public static async Task UpdateAsync_UpdatesOrderingParty(
+        public static async Task UpdateAsync_UpdatesSupplierDetails(
             [Frozen] Mock<IContactDetailsService> contactDetailsService,
-            [Frozen] ApplicationDbContext context,
+            [Frozen] Mock<ISupplierSectionService> supplierSectionService,
             [Frozen] CallOffId callOffId,
             Order order,
             SupplierModel model,
             SupplierSectionController controller)
         {
             order.Supplier.Should().NotBeEquivalentTo(model);
-
-            context.Order.Add(order);
-            await context.SaveChangesAsync();
+            supplierSectionService.Setup(o => o.GetOrder(callOffId)).ReturnsAsync(order);
+            supplierSectionService.Setup(o => o.SetSupplierSection(order, It.IsAny<Supplier>(), It.IsAny<Contact>())).Callback(() =>
+            {
+                order.Supplier = new Supplier { Name = model.Name };
+            });
 
             contactDetailsService
                 .Setup(s => s.AddOrUpdateAddress(It.IsNotNull<Address>(), It.IsNotNull<AddressModel>()))
@@ -110,53 +129,78 @@ namespace NHSD.BuyingCatalogue.Ordering.Api.UnitTests.Controllers
 
             await controller.UpdateAsync(callOffId, model);
 
-            order.Supplier.Should().BeEquivalentTo(model, o => o.Including(p => p.Name));
+            order.Supplier.Should().BeEquivalentTo(model, o => o.Including(s => s.Name));
+        }
+
+        [Test]
+        [InMemoryDbAutoData]
+        public static async Task UpdateAsync_UpdatesSupplierContact(
+            [Frozen] Mock<IContactDetailsService> contactDetailsService,
+            [Frozen] Mock<ISupplierSectionService> supplierSectionService,
+            [Frozen] CallOffId callOffId,
+            Order order,
+            SupplierModel model,
+            SupplierSectionController controller)
+        {
+            var expectedContact = order.SupplierContact;
+
+            order.Supplier.Should().NotBeEquivalentTo(model);
+            supplierSectionService.Setup(o => o.GetOrder(callOffId)).ReturnsAsync(order);
+
+            contactDetailsService
+                .Setup(s => s.AddOrUpdatePrimaryContact(It.IsNotNull<Contact>(), It.IsNotNull<PrimaryContactModel>()))
+                .Returns(order.SupplierContact);
+
+            await controller.UpdateAsync(callOffId, model);
+
+            expectedContact.Should().BeEquivalentTo(order.SupplierContact);
         }
 
         [Test]
         [InMemoryDbAutoData]
         public static async Task UpdateAsync_InvokesAddOrUpdateAddress(
             [Frozen] Mock<IContactDetailsService> contactDetailsService,
-            [Frozen] ApplicationDbContext context,
+            [Frozen] Mock<ISupplierSectionService> supplierSectionService,
             [Frozen] CallOffId callOffId,
             Order order,
             SupplierModel model,
             SupplierSectionController controller)
         {
-            context.Order.Add(order);
-            await context.SaveChangesAsync();
-
             var originalAddress = order.Supplier.Address;
 
             Expression<Func<IContactDetailsService, Address>> addOrUpdateAddress = s => s.AddOrUpdateAddress(
                 It.Is<Address>(a => a == originalAddress),
                 It.Is<AddressModel>(a => a == model.Address));
 
+            supplierSectionService.Setup(o => o.GetOrder(callOffId)).ReturnsAsync(order);
+            supplierSectionService.Setup(o => o.SetSupplierSection(order, It.IsAny<Supplier>(), It.IsAny<Contact>())).Callback(() =>
+            {
+                order.Supplier.Address = originalAddress;
+            });
             contactDetailsService.Setup(addOrUpdateAddress).Returns(originalAddress);
 
             await controller.UpdateAsync(callOffId, model);
-
             contactDetailsService.Verify(addOrUpdateAddress);
+
+            order.Supplier.Address.Should().BeEquivalentTo(originalAddress);
         }
 
         [Test]
         [InMemoryDbAutoData]
         public static async Task UpdateAsync_InvokesAddOrUpdatePrimaryContact(
             [Frozen] Mock<IContactDetailsService> contactDetailsService,
-            [Frozen] ApplicationDbContext context,
+            [Frozen] Mock<ISupplierSectionService> supplierSectionService,
             [Frozen] CallOffId callOffId,
             Order order,
             SupplierModel model,
             SupplierSectionController controller)
         {
-            context.Order.Add(order);
-            await context.SaveChangesAsync();
-
-            contactDetailsService
-                .Setup(s => s.AddOrUpdateAddress(It.IsNotNull<Address>(), It.IsNotNull<AddressModel>()))
-                .Returns(order.Supplier.Address);
-
             var originalContact = order.SupplierContact;
+
+            supplierSectionService.Setup(o => o.GetOrder(callOffId)).ReturnsAsync(order);
+            contactDetailsService
+                .Setup(s => s.AddOrUpdatePrimaryContact(It.IsNotNull<Contact>(), It.IsNotNull<PrimaryContactModel>()))
+                .Returns(order.SupplierContact);
 
             await controller.UpdateAsync(callOffId, model);
 
@@ -167,41 +211,15 @@ namespace NHSD.BuyingCatalogue.Ordering.Api.UnitTests.Controllers
 
         [Test]
         [InMemoryDbAutoData]
-        public static async Task UpdateAsync_SavesToDb(
-            [Frozen] Mock<IContactDetailsService> contactDetailsService,
-            [Frozen] ApplicationDbContext context,
-            [Frozen] CallOffId callOffId,
-            Order order,
-            SupplierModel model,
-            SupplierSectionController controller)
-        {
-            context.Order.Add(order);
-            await context.SaveChangesAsync();
-
-            contactDetailsService
-                .Setup(s => s.AddOrUpdateAddress(It.IsNotNull<Address>(), It.IsNotNull<AddressModel>()))
-                .Returns(order.Supplier.Address);
-
-            await controller.UpdateAsync(callOffId, model);
-
-            context.Set<Order>().First(o => o.Equals(order)).Supplier.Should().BeEquivalentTo(
-                model,
-                o => o.Including(p => p.Name));
-        }
-
-        [Test]
-        [InMemoryDbAutoData]
         public static async Task UpdateAsync_SuccessfulUpdate_ReturnsNoContentResult(
             [Frozen] Mock<IContactDetailsService> contactDetailsService,
-            [Frozen] ApplicationDbContext context,
+            [Frozen] Mock<ISupplierSectionService> supplierSectionService,
             [Frozen] CallOffId callOffId,
             Order order,
             SupplierModel model,
             SupplierSectionController controller)
         {
-            context.Order.Add(order);
-            await context.SaveChangesAsync();
-
+            supplierSectionService.Setup(o => o.GetOrder(callOffId)).ReturnsAsync(order);
             contactDetailsService
                 .Setup(s => s.AddOrUpdateAddress(It.IsNotNull<Address>(), It.IsNotNull<AddressModel>()))
                 .Returns(order.Supplier.Address);
