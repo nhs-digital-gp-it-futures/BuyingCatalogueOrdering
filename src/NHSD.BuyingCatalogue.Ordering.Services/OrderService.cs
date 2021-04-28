2b9f8506aa8d2c2cf7c0587f356914325955ad82
RahulJha-mastek <Rahul.Jha@mastek.com>
2021-04-28 12:32:35 +0100

    update with review comment

--- src/NHSD.BuyingCatalogue.Ordering.Services/OrderService.cs
+++ src/NHSD.BuyingCatalogue.Ordering.Services/OrderService.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using JetBrains.Annotations;
 using Microsoft.EntityFrameworkCore;
 using NHSD.BuyingCatalogue.Ordering.Contracts;
 using NHSD.BuyingCatalogue.Ordering.Domain;
